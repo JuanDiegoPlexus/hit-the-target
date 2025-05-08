13cd878bbb25bdd96c4478e259c62ce8c7c719f9
JuanDiegoPlexus <juan.touchardruiz@plexus.es>
2025-05-08 10:31:48 +0200

    Husky correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BannerComponent } from './components/background/banner/banner.component';
 
 @Component({
   selector: 'app-root',
